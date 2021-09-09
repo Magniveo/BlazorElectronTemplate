ef895cb2808133f75c7c1fa0667fe4302b85990d
magniveo <michailanatger@mail.ru>
2021-09-09 16:28:38 +0500

    Load project

---
+++ Components/TabClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorElectronTemplate.Components
+{
+    public class TabClass
+    {
+        public string Name { get; set; }
+        public string Href { get; set; }
+        public string Icon { get; set; }
+    }
+}
